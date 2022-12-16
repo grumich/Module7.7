d353f4e330179f8ad4d4aae368ad54fe8fcfc2ce
Alex <43243265+grumich@users.noreply.github.com>
2022-12-16 08:39:37 +0300

    Update init.js

--- bjs/10_function_object/init.js
+++ bjs/10_function_object/init.js
@@ -1,7 +1,22 @@
+document.querySelector('#generation').addEventListener('click', function() {
+const initPerson = personGenerator.getPerson();
+document.querySelector('#firstNameOutput').innerText = initPerson.firstName;
+document.querySelector('#surnameOutput').innerText = initPerson.surName;
+document.querySelector('#secondNameOutput').innerText = initPerson.patroNymic;
+document.querySelector('#genderOutput').innerText = initPerson.gender;
+document.querySelector('#birthYearOutput').innerText = initPerson.yearOfBirthday;
+document.querySelector('#professionOutput').innerText = initPerson.profession;
+document.querySelector('#birthDayOutput').innerText = initPerson.day;
+document.querySelector('#birthMonthOutput').innerText = initPerson.month;
+});
 
-window.onload = function()
-{
-    const initPerson = personGenerator.getPerson();
-    document.getElementById('firstNameOutput').innerText = initPerson.firstName;
-};
-
+document.querySelector('#clear').addEventListener('click', function() {
+document.querySelector('#firstNameOutput').innerText = "Имя";
+document.querySelector('#surnameOutput').innerText = "Фамилия";
+document.querySelector('#secondNameOutput').innerText = "Отчество";
+document.querySelector('#genderOutput').innerText = "Пол";
+document.querySelector('#birthDayOutput').innerText = "";
+document.querySelector('#birthMonthOutput').innerText = "";
+document.querySelector('#birthYearOutput').innerText = "Дата рождения,";
+document.querySelector('#professionOutput').innerText = "Профессия";
+});
